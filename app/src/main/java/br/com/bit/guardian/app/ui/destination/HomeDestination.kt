81c4ad8c8c27745317894396ccab2121e12e1bba
Diego Nascimento <nascimento.diegof@gmail.com>
2024-11-28 15:49:57 -0300

    [GUARD-26] Imnplement navigation adaptive. (#32)

--- app/src/main/java/br/com/bit/guardian/app/ui/destination/HomeDestination.kt
+++ app/src/main/java/br/com/bit/guardian/app/ui/destination/HomeDestination.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import br.com.bit.guardian.core.designsystem.theme.GuardianTheme
 
 @Composable
 fun HomeDestination(){
-    Box(modifier = Modifier.fillMaxSize().background(Color.Green)){
+    Box(modifier = Modifier.fillMaxSize().background(GuardianTheme.colors.surface)){
 
     }
 }
