7d542e63bffdc1f9a0b1d77c0746826454c833d0
Diego Nascimento <nascimento.diegof@gmail.com>
2026-04-11 19:23:45 -0300

    adjust gitattributs configuration.

--- build-config/convention/src/main/java/br/com/bit/guardian/convention/core/GuardianBuildType.kt
+++ build-config/convention/src/main/java/br/com/bit/guardian/convention/core/GuardianBuildType.kt
@@ -3,7 +3,6 @@ package br.com.bit.guardian.convention.core
 @Suppress("unused")
 enum class GuardianBuildType(val applicationIdSuffix: String? = null) {
     DEBUG(".debug"),
-    TEST(".test"),
     RELEASE,
     BENCHMARK(".benchmark")
 }
