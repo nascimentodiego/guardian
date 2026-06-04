19c0725b4ac0be3b0ca3b657e4d5378e9fd9993e
Diego Nascimento <nascimento.diegof@gmail.com>
2026-06-04 05:05:48 -0300

    adjust sonar properties.

--- build-config/convention/src/main/java/AndroidSonarConventionPlugin.kt
+++ build-config/convention/src/main/java/AndroidSonarConventionPlugin.kt
@@ -38,11 +38,11 @@ class AndroidSonarConventionPlugin : Plugin<Project> {
 
                     property(
                         "sonar.coverage.jacoco.xmlReportPaths",
-                        "**/build/reports/coverage/jacoco/**/*.xml",
+                        "build/reports/coverage/jacoco/debug.xml",
                     )
                     property(
                         "sonar.junit.reportPaths",
-                        "**/build/test-results/**/TEST-*.xml",
+                        "build/test-results/testDebugUnitTest",
                     )
 
                     property(
