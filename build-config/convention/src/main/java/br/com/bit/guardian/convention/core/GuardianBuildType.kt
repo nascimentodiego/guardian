1f79ff431ed75afc40542921a89facf6cc300b05
Diego Nascimento <nascimento.diegof@gmail.com>
2026-09-16 12:57:11 -0300

    Feature/guard 65 implement pre release flow (#77)
    * fix config.
    
    * fix config.

--- build-config/convention/src/main/java/br/com/bit/guardian/convention/core/GuardianBuildType.kt
+++ build-config/convention/src/main/java/br/com/bit/guardian/convention/core/GuardianBuildType.kt
@@ -3,6 +3,7 @@ package br.com.bit.guardian.convention.core
 @Suppress("unused")
 enum class GuardianBuildType(val applicationIdSuffix: String? = null) {
     DEBUG(".debug"),
+    HOMOLOG(".homolog"),
     RELEASE,
     BENCHMARK(".benchmark")
 }
